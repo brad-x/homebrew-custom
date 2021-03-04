b5290b46ef1ceb9e63229be3986c1143eac04982
Brad Laue <blaue@uncharted.software>
2021-03-03 23:41:52 -0500

    Try meson instead

--- spice-server.rb
+++ spice-server.rb
@@ -7,9 +7,8 @@ class SpiceServer < Formula
   sha256 "551d4be4a07667cf0543f3c895beb6da8a93ef5a9829f2ae47817be5e616a114"
   license "BSD"
 
-  depends_on "autoconf" => :build
-  depends_on "autogen" => :build
-  depends_on "automake" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
 
   depends_on "glib"
   depends_on "pixman"
@@ -18,25 +17,26 @@ class SpiceServer < Formula
   depends_on "spice-protocol"
 
   def install
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@1.1"].opt_lib/"pkgconfig"
-    system "autoreconf"
-    system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, "-Dwith-docs=false", ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   patch do
-    url "https://raw.githubusercontent.com/brad-x/homebrew-custom/main/patches/spice-server/red-worker.patch"
-    sha256 "67e3c7cf21249991cf3f9f6ba136bc90a11ce7dbff678764eb17b6b2bac9ffd7"
+    url "https://github.com/brad-x/homebrew-custom/raw/main/patches/spice-server/pthread-setname-args.diff"
+    sha256 "7a0dd200792d03839ca68de4e44edce45d007b6b479fcf1dbc7625b3cdeae444"
   end
 
   patch do
-    url "https://raw.githubusercontent.com/brad-x/homebrew-custom/main/patches/spice-server/test-websocket.patch"
-    sha256 "c7407e2f4797b48ad05acc94971147ff65945ad5b2fc5d613785a3c294576e98"
+    url "https://github.com/brad-x/homebrew-custom/raw/main/patches/spice-server/no-werror.diff"
+    sha256 "5927d53e7037580a54f26c01d9727057485987b8e4555402cce79bbf0f0709da"
   end
-  
+
   patch do
-    url "https://raw.githubusercontent.com/brad-x/homebrew-custom/main/patches/spice-server/no-tests-tools.patch"
-    sha256 "5a99af15d5f18775f8a9b061684074c269815d28753d5a933554cf416b062bbb"
+    url "https://github.com/brad-x/homebrew-custom/raw/main/patches/spice-server/no-tests.diff"
+    sha256 "999c5415f1e07573b1fea95b053efb078bb44999af10a6476db13ea4eb7f4736"
   end
 
   test do
