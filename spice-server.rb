7606f9942b62d3c2674369a6630b2937d454d0e4
Brad Laue <blaue@uncharted.software>
2021-03-03 20:50:35 -0500

    autoreconf

--- spice-server.rb
+++ spice-server.rb
@@ -1,6 +1,5 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SpiceServer < Formula
   desc ""
   homepage ""
@@ -8,26 +7,14 @@ class SpiceServer < Formula
   sha256 "551d4be4a07667cf0543f3c895beb6da8a93ef5a9829f2ae47817be5e616a114"
   license ""
 
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
     system "./configure", "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    system "autoreconf"
     system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test spice-0.14.3.tar.bz`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
