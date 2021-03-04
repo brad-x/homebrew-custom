70d1bc130543d42eae0ec800ae0e70c30b58312d
Brad Laue <blaue@uncharted.software>
2021-03-03 21:08:46 -0500

    Put autoreconf in the right place

--- spice-server.rb
+++ spice-server.rb
@@ -1,21 +1,22 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class SpiceServer < Formula
-  desc ""
-  homepage ""
+  desc "Remote virtual machine display server"
+  homepage "https://www.spice-space.org/"
   url "https://www.spice-space.org/download/releases/spice-server/spice-0.14.3.tar.bz2"
   sha256 "551d4be4a07667cf0543f3c895beb6da8a93ef5a9829f2ae47817be5e616a114"
-  license ""
+  license "BSD"
 
   depends_on "autoconf" => :build
   depends_on "autogen" => :build
   depends_on "automake" => :build
 
+  depends_on "glib"
   depends_on "spice-protocol"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
     system "autoreconf"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 
