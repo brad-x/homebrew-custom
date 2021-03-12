dadcc8d384041bc23face3f71ae7cc90a6eebb9b
Brad Laue <blaue@uncharted.software>
2021-03-12 12:59:14 -0500

    autoreconf

--- libapplewm.rb
+++ libapplewm.rb
@@ -5,6 +5,9 @@ class Libapplewm < Formula
   sha256 "a973c40cc48e5efab9ab295ce497f746891b2fe59b274aa08010b8a7c99e96f8"
   license "NOASSERTION"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on "util-macros" => :build
 
@@ -20,6 +23,7 @@ class Libapplewm < Formula
       --disable-silent-rules
     ]
 
+    system "autoreconf", "-fvi"
     system "./configure", *args
     system "make", "install"
   end
