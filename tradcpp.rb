0e74ea6994b0fd733bb9d498d36ee3e7586b4486
Brad Laue <blaue@uncharted.software>
2021-03-12 14:36:03 -0500

    Add tradcpp

---
+++ tradcpp.rb
@@ -0,0 +1,14 @@
+class Tradcpp < Formula
+  desc "a traditional (K&R-style) C preprocessor"
+  homepage "http://ftp.netbsd.org/pub/NetBSD/misc/dholland/"
+  url "http://ftp.netbsd.org/pub/NetBSD/misc/dholland/tradcpp-0.5.tar.gz"
+  sha256 "f03fea0d8803f23ae1fd2fa552332e26966470844da219587ba6c604d02a4dc4"
+  license "BSD"
+
+  depends_on "bsdmake" => :build
+
+  def install
+    system "bsdmake", "install"
+  end
+
+end
