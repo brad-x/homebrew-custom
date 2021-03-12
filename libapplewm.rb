a923c600f4e723ebfb48af13a66a29bd0ceeee89
Brad Laue <blaue@uncharted.software>
2021-03-12 12:54:57 -0500

    fix?

--- libapplewm.rb
+++ libapplewm.rb
@@ -5,6 +5,9 @@ class Libapplewm < Formula
   sha256 "a973c40cc48e5efab9ab295ce497f746891b2fe59b274aa08010b8a7c99e96f8"
   license "NOASSERTION"
 
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "util-macros" => :build
+
   depends_on "libxext"
   depends_on "xorgproto"
 
