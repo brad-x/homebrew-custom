426bb372d78c2dd51776bcc32d8fc522d13e6904
Brad Laue <brad@brad-x.com>
2021-11-07 16:03:04 -0500

    Add version

--- vde.rb
+++ vde.rb
@@ -2,7 +2,7 @@ class Vde < Formula
   desc "Ethernet compliant virtual network"
   homepage "https://vde.sourceforge.io/"
   url "https://github.com/virtualsquare/vde-2/tarball/534a09453f956f8f545ba4fe37413be1a139314d"
-  version="534a"
+  version="2.3.3a"
   sha256 "2955839f6d0ccac3e17c9aeec88204ce78bc93c846ace9b323e5e69e6f090a95"
   license "GPL-2.0"
   revision 1
