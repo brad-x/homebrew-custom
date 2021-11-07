dd42db7367a764e60fd11c7654fc4e9718f11d4a
Brad Laue <brad@brad-x.com>
2021-11-07 16:09:54 -0500

    I cannot read

--- vde.rb
+++ vde.rb
@@ -3,7 +3,7 @@ class Vde < Formula
   homepage "https://vde.sourceforge.io/"
   url "https://github.com/virtualsquare/vde-2/tarball/534a09453f956f8f545ba4fe37413be1a139314d"
   sha256 "2955839f6d0ccac3e17c9aeec88204ce78bc93c846ace9b323e5e69e6f090a95"
-  version="2.3.3"
+  version "534a"
   license "GPL-2.0"
   revision 1
 
