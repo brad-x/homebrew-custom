b6cff93e28b7e2f896b7cced84fc0c59adc0e02a
Brad Laue <brad@brad-x.com>
2023-07-08 19:36:45 -0400

    Bump qemu

--- qemu-utm.rb
+++ qemu-utm.rb
@@ -1,8 +1,8 @@
 class QemuUtm < Formula
   desc "Emulator for x86 and PowerPC with UTM patchset"
   homepage "https://www.qemu.org/"
-  url "https://github.com/utmapp/qemu/releases/download/v8.0.0-utm/qemu-8.0.0-utm.tar.bz2"
-  sha256 "42ad99f6b42b5e38fcde7c422aaac7609778ab5b7f540e23da12df11db2c0a1a"
+  url "https://github.com/utmapp/qemu/releases/download/v8.0.2-utm/qemu-8.0.2-utm.tar.bz2"
+  sha256 "d12dab87e60c2f4bb7b6f39fc2af24a5d3d6cbdeac022553ae08e9cee2384baf"
   license "GPL-2.0-only"
   head "https://github.com/utmapp/qemu.git", branch: "utm-edition"
 
@@ -44,7 +44,7 @@ class QemuUtm < Formula
 
   patch do
     url "https://github.com/brad-x/homebrew-custom/raw/main/patches/qemu-utm/qemu-utm.diff"
-    sha256 "43ea021e9b47210b013bd7924a77d43520fad9672f6d369d7e425f643c8225d1"
+    sha256 "2703b422c620984b6cee428a45b8a56f313592b236a9622def735f02da0ffed2"
   end
 
   patch do
