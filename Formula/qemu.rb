d9ddb6d9e8a76f8e7671a7c3ae408725aa26b217
Brad Laue <brad@brad-x.com>
2023-09-02 09:59:42 -0400

    Revert to 7.2.0

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -1,8 +1,8 @@
 class Qemu < Formula
   desc "Emulator for x86 and PowerPC"
   homepage "https://www.qemu.org/"
-  url "https://download.qemu.org/qemu-8.0.2.tar.xz"
-  sha256 "f060abd435fbe6794125e2c398568ffc3cfa540042596907a8b18edca34cf6a5"
+  url "https://download.qemu.org/qemu-7.2.0.tar.xz"
+  sha256 "5b49ce2687744dad494ae90a898c52204a3406e84d072482a1e1be854eeb2157"
   license "GPL-2.0-only"
   head "https://git.qemu.org/git/qemu.git", branch: "master"
 
